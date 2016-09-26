cad78efa9c19d619b615336c9025273d8dc532f4
jtwu <442952891@qq.com>
2016-09-26 19:29:36 +0800

    fix

--- core/src/main/java/com/github/jyoghurt/core/exception/ServiceException.java
+++ core/src/main/java/com/github/jyoghurt/core/exception/ServiceException.java
@@ -1,14 +1,13 @@
 package com.github.jyoghurt.core.exception;
 
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * Created by IntelliJ IDEA. User: jtwu Date: 12-12-5 Time: 上午10:46 Service层异常
  */
 public class ServiceException extends BaseException {
-	private static final long serialVersionUID = 2657618496024743577L;
+    private static final long serialVersionUID = 2657618496024743577L;
 
-	public ServiceException() {
+    public ServiceException() {
         super();
     }
 
@@ -17,11 +16,18 @@ public class ServiceException extends BaseException {
     }
 
     public ServiceException(Throwable cause) {
-        super(cause.getMessage(),cause);
+        super(cause.getMessage(), cause);
     }
 
     public ServiceException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    public ServiceException(ExceptionBody exceptionBody) {
+        super(exceptionBody);
+    }
+
+    public ServiceException(ExceptionBody exceptionBody, Throwable cause) {
+        super(exceptionBody, cause);
+    }
 }
